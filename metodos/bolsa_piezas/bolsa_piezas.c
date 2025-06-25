0087a5e986d1ffa2c1fb8811aa90ec3f6821b795
Pruden <prudencosta@gmail.com>
2025-06-25 19:47:33 +0200

    Bolsa de piezas

---
+++ metodos/bolsa_piezas/bolsa_piezas.c
@@ -0,0 +1,50 @@
+//
+// Created by pruden on 25/06/25.
+//
+
+#include "bolsa_piezas.h"
+
+#include <stdlib.h>
+
+#include "constantes.h"
+#include "Pieza.h"
+#include "Pieza_T.h"
+
+#define NUM_TIPOS_PIEZA 1
+Pieza *bolsa[NUM_TIPOS_PIEZA];
+int bolsa_size = 0;
+
+void cargar_bolsa();
+
+
+Pieza *devolver_pieza_aleatoria() {
+    if (bolsa_size == 0) {
+        cargar_bolsa();
+    }
+
+    return bolsa[--bolsa_size];
+}
+
+
+void cargar_bolsa() {
+    int columnaInicial = COLUMNAS / 2;
+    //bolsa[0] = (Pieza *)crear_pieza_I(0, columnaInicial);
+    //bolsa[1] = (Pieza *)crear_pieza_O(0, columnaInicial);
+    bolsa[0] = (Pieza *)crear_pieza_T(0, columnaInicial);
+    //bolsa[3] = (Pieza *)crear_pieza_S(0, columnaInicial);
+    //bolsa[4] = (Pieza *)crear_pieza_Z(0, columnaInicial);
+    //bolsa[5] = (Pieza *)crear_pieza_L(0, columnaInicial);
+    //bolsa[6] = (Pieza *)crear_pieza_J(0, columnaInicial);
+
+    bolsa_size = NUM_TIPOS_PIEZA;
+
+    /*
+    // Mezclar (Fisher-Yates)
+
+    for (int i = NUM_TIPOS_PIEZA - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        Pieza *tmp = bolsa[i];
+        bolsa[i] = bolsa[j];
+        bolsa[j] = tmp;
+    }*/
+}
---
+++ metodos/bolsa_piezas/bolsa_piezas.h
@@ -0,0 +1,10 @@
+//
+// Created by pruden on 25/06/25.
+//
+
+#ifndef BOLSA_PIEZAS_H
+#define BOLSA_PIEZAS_H
+#include "Pieza.h"
+
+Pieza *devolver_pieza_aleatoria();
+#endif //BOLSA_PIEZAS_H
